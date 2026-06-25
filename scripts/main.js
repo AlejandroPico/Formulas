978b2eaee1233afbd0c75c7839ecfde8b60d79cc
Alejandro Pico <115283749+AlejandroPico@users.noreply.github.com>
2026-06-25 12:34:41 +0200

    Remove splash initialization

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,6 @@ import { finalCorrections } from "../data/final-corrections.js";
 import { state, setState } from "./state.js";
 import { $, unique } from "./utils.js";
 import { filterEquations } from "./filtering.js";
-import { drawHeroCanvas } from "./simulations.js";
 import { renderEquationGrid, openEquationModal, closeEquationModal } from "./render.js";
 import { initTheme } from "./theme.js";
 
@@ -22,8 +21,6 @@ function boot() {
   initTheme($("#themeToggle"));
   initFilterControls();
   bindEvents();
-  drawHeroCanvas($("#heroCanvas"));
-  initSplash();
   renderAll();
 }
 
@@ -63,16 +60,6 @@ function bindEvents() {
   $("#equationModal").addEventListener("click", event => {
     if (event.target.id === "equationModal") closeEquationModal();
   });
-}
-
-function initSplash() {
-  const splash = $("#splashScreen");
-  function hideSplash() {
-    splash.classList.add("is-hidden");
-  }
-
-  splash.addEventListener("click", hideSplash, { once: true });
-  window.setTimeout(hideSplash, 2200);
 }
 
 function renderAll() {
